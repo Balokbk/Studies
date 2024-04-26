93aa484ce78d083c1a6c173e719cda29cc4eb0c4
Balo <166945987+Balokbk@users.noreply.github.com>
2024-04-26 20:52:11 -0300

    Update Decimal to binary or octanary or hexadecimal.c
    Updated some things to the code to work a little bit better.

--- Decimal to binary or octanary or hexadecimal.c
+++ Decimal to binary or octanary or hexadecimal.c
@@ -3,11 +3,12 @@
 int main() {
     const char baseDigits[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
     int convertedNum[64];
-    long int numToConvert;
+    long int numToConvert, fixedNum;
     int nextDigit, base, i = 0;
 
+    fixedNum = numToConvert;
     //Receives the number and base.
-
+    printf("Insert a number and choose the base (2, 8 or 16).\n");
     scanf("%ld %d", &numToConvert, &base);
 
     if(base == 2 || base == 8 ||base == 16){
@@ -22,14 +23,14 @@ int main() {
 
     //Shows the number in the inverse order.  
 
-    printf("Converted number %ld to base %d = ", numToConvert, base);
+    printf("Converted number %ld to base %d = ", fixedNum, base);
     for(i--; i >= 0; i--){
         nextDigit = convertedNum[i];
-        printf("%c", baseDigits[nextDigit]);
+        printf("%c\n", baseDigits[nextDigit]);
     }
     }
     else{
-        printf("Use outra base!");
+        printf("Use other base(2, 8 or 16)!\n");
     }
     return 0;
 }
